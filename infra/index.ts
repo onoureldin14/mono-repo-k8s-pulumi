0c598b9f3c86d6ee4fb486e72503b36454226024
omarnour14 <omar.din@youlend.com>
2025-09-02 08:30:57 +0100

    simplifying the infra

--- infra/index.ts
+++ infra/index.ts
@@ -1,39 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as docker from "@pulumi/docker";
 import * as k8s from "@pulumi/kubernetes";
-
-const stack = pulumi.getStack();
-const config = new pulumi.Config();
 
 // Configuration values
 const appName = "simple-typescript-app-pulumi";
 const imageTag = "latest";
 const containerPort = 3000;
+const nodePort = 30000;
 const replicas = 2;
 
-// Docker registry configuration
-const registryUrl = "onoureldin14";
-
-// Use the pre-built image from Docker Hub
-const image = new docker.RemoteImage(`${appName}-image`, {
-    name: `${registryUrl}/${appName}:latest`,
-});
-
-// Create Kubernetes namespace
-const namespace = new k8s.core.v1.Namespace(`${appName}-namespace`, {
+// Kubernetes Deployment
+const deployment = new k8s.apps.v1.Deployment(appName, {
     metadata: {
         name: appName,
-        labels: {
-            app: appName,
-        },
-    },
-});
-
-// Create Kubernetes deployment
-const deployment = new k8s.apps.v1.Deployment(`${appName}-deployment`, {
-    metadata: {
-        name: appName,
-        namespace: namespace.metadata.name,
         labels: {
             app: appName,
         },
@@ -54,13 +32,16 @@ const deployment = new k8s.apps.v1.Deployment(`${appName}-deployment`, {
             spec: {
                 containers: [{
                     name: appName,
-                    image: image.name,
+                    image: `onoureldin14/${appName}:${imageTag}`,
                     ports: [{
                         containerPort: containerPort,
                     }],
                     env: [{
                         name: "NODE_ENV",
                         value: "production",
+                    }, {
+                        name: "PORT",
+                        value: containerPort.toString(),
                     }],
                     resources: {
                         requests: {
@@ -92,67 +73,30 @@ const deployment = new k8s.apps.v1.Deployment(`${appName}-deployment`, {
             },
         },
     },
-}, { dependsOn: [namespace] });
+});
 
-// Create Kubernetes service
+// Kubernetes Service
 const service = new k8s.core.v1.Service(`${appName}-service`, {
     metadata: {
-        name: appName,
-        namespace: namespace.metadata.name,
+        name: `${appName}-service`,
         labels: {
             app: appName,
         },
     },
     spec: {
-        type: "ClusterIP",
-        ports: [{
-            port: 80,
-            targetPort: containerPort,
-            protocol: "TCP",
-        }],
+        type: "NodePort",
         selector: {
             app: appName,
         },
-    },
-}, { dependsOn: [deployment] });
-
-// Create Kubernetes ingress (optional - for external access)
-const ingress = new k8s.networking.v1.Ingress(`${appName}-ingress`, {
-    metadata: {
-        name: appName,
-        namespace: namespace.metadata.name,
-        labels: {
-            app: appName,
-        },
-        annotations: {
-            "kubernetes.io/ingress.class": "nginx",
-            "nginx.ingress.kubernetes.io/rewrite-target": "/",
-        },
-    },
-    spec: {
-        rules: [{
-            host: config.get("ingressHost") || `${appName}.local`,
-            http: {
-                paths: [{
-                    path: "/",
-                    pathType: "Prefix",
-                    backend: {
-                        service: {
-                            name: service.metadata.name,
-                            port: {
-                                number: 80,
-                            },
-                        },
-                    },
-                }],
-            },
+        ports: [{
+            port: containerPort,
+            targetPort: containerPort,
+            nodePort: nodePort,
         }],
     },
-}, { dependsOn: [service] });
+});
 
 // Export important values
-export const imageName = image.name;
-export const namespaceName = namespace.metadata.name;
+export const deploymentName = deployment.metadata.name;
 export const serviceName = service.metadata.name;
-export const ingressName = ingress.metadata.name;
-export const appUrl = config.get("ingressHost") || `${appName}.local`;
+export const appUrl = `http://$(minikube ip):${nodePort}`;
